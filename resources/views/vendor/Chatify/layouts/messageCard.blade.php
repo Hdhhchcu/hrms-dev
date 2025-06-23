9aa19f42e409e308883d2a6c857a8e470f9dac1d
Gyanender <gyanender@codleo.com>
2025-06-23 17:33:36 +0530

    Added laravel chatapp

---
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -0,0 +1,57 @@
+<?php
+$seenIcon = (!!$seen ? 'check-double' : 'check');
+// $timeAndSeen = "<span data-time='$created_at' class='message-time'>
+//         ".($isSender ? "<span class='fas fa-$seenIcon' seen'></span>" : '' )." <span class='time'>$timeAgo</span>
+//     </span>";
+$timeAndSeen = "<span data-time='$created_at' class='message-time'>
+        ".($isSender ? "<span class='fas fa-$seenIcon' seen'></span>" : '' )." <span >$msg_time</span></span>";
+?>
+<div class="message-card-wrapper @if($isSender) mc-sender @endif">
+    <div class="message-user">
+         {{$msg_date}}
+    </div>
+    @if($loadUserInfo)
+    <div class="message-user">
+        <img src="{{$user->avatar}}"/>
+        <p>{{$user->name}}</p>
+    </div>
+    @endif
+    <div class="message-card @if($isSender) mc-sender @endif" data-id="{{ $id }}">
+        {{-- Delete Message Button --}}
+        @if ($isSender)
+            <div class="actions">
+                <i class="fas fa-trash delete-btn" data-id="{{ $id }}"></i>
+            </div>
+            
+            @if ((time()-(strtotime($created_at)))<=600) 
+            <div class="actions">
+                <i class="fas fa-edit edit_btn" data-id="{{ $id }}" ></i>
+            </div> 
+            @endif
+        @endif
+        {{-- Card --}}
+        <div class="message-card-content">
+            @if (@$attachment->type != 'image' || $message)
+                <div class="message">
+                    <span class="type">{!! ($message == null && $attachment != null && @$attachment->type != 'file') ? $attachment->title : nl2br($message) !!}</span>
+                    {!! $timeAndSeen !!}
+                    {{-- If attachment is a file --}}
+                    @if(@$attachment->type == 'file')
+                        <a href="{{ route(config('chatify.attachments.download_route_name'), ['fileName'=>$attachment->file]) }}" class="file-download">
+                            <span class="fas fa-file"></span> {{$attachment->title}}</a>
+                    @endif
+                </div>
+            @endif
+            @if(@$attachment->type == 'image')
+                <div class="image-wrapper" style="text-align: {{$isSender ? 'end' : 'start'}}">
+                    <div class="image-file chat-image" style="background-image: url('{{ Chatify::getAttachmentUrl($attachment->file) }}')">
+                        <div>{{ $attachment->title }}</div>
+                    </div>
+                    <div style="margin-bottom:5px">
+                        {!! $timeAndSeen !!}
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
